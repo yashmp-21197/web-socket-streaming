76c0cc0caebe451374f0eb80ef76e52c2d51244b
Yash Patel <yashmp.21197@gmail.com>
2025-01-29 18:10:44 -0600

    code reformat

--- client/web-socket/index.js
+++ client/web-socket/index.js
@@ -1,162 +1,329 @@
-$(document).ready(function() {
-    const video = document.getElementById("videoPlayer");
-    const status = document.getElementById("status");
-    const streamingBtn = document.getElementById("streamingBtn");
-
-    let isStreaming = false;
-    let mediaSource;
-    let sourceBuffer;
-    let ws;
-    let start = 1;
-    let end = 10;
-    let current = start;
-    let duration = 5.12
-    let startTime = -(duration * (start-1));
-    let maxTime = duration * (end-start+1);
-
-    streamingBtn.addEventListener("click", () => {
-        if(isStreaming){
-            stopStreaming();
-        }else{
-            startStreaming();
-        }
-    });
-
-    function initializeMediaSource() {
-        mediaSource = new MediaSource();
-
-        video.src = URL.createObjectURL(mediaSource);
-
-        mediaSource.addEventListener("sourceopen", () => {
-            console.log("MediaSource is open");
-            const mimeCodec = 'video/mp4; codecs="avc1.640028"';
-            sourceBuffer = mediaSource.addSourceBuffer(mimeCodec);
-            console.log("SourceBuffer added:", mimeCodec);
-
-            sourceBuffer.addEventListener("updateend", () => {
-                // mediaSource.endOfStream();
-                // video.play();
-                console.log(mediaSource.readyState);
-            });
-
-            // video.addEventListener('timeupdate', checkBuffer);
-
-            video.addEventListener('canplay', function () {
-                video.play();
-            });
-
-            // video.addEventListener('seeking', (e) => {
-            //     console.log(e);
-            //     if (mediaSource.readyState === 'open') {
-            //         sourceBuffer.abort();
-            //         console.log(mediaSource.readyState);
-            //     } else {
-            //         console.log('seek but not open?');
-            //         console.log(mediaSource.readyState);
-            //     }
-            // });
-
-            sourceBuffer.addEventListener("error", (e) => {
-                console.error("SourceBuffer error:", e);
-            });
-
-            sourceBuffer.timestampOffset = startTime;
-            mediaSource.duration = maxTime;
-            
-            // ws.send(JSON.stringify({
-            //     type: "segment",
-            //     segment: current.toString().padStart(3, '0') + ".m4s"
-            // }));
-            ws.send(JSON.stringify({
-                type: "segment",
-                segment: "init.mp4"
-            }));
-        });
-
-        mediaSource.addEventListener("sourceended", () => {
-            console.log("MediaSource ended");
-        });
+$(document).ready(function() { 
+    
+    isDefined = (v) => {
+        return typeof v !== 'undefined';
     }
 
-    function deinitializeMediaSource() {
-        if (mediaSource) {
-            if (sourceBuffer && !sourceBuffer.updating) {
+    isUndefined = (v) => {
+        return typeof v === 'undefined';
+    }
+
+    class WS {
+        constructor (url) {
+            this.vpDict = {};
+            this.url = url;
+            this.ws = null;
+            this.resetReconnect();
+        }
+
+        static getInstance = (url) => {
+            if (!WS.wsDict) {
+                WS.wsDict = {};
+            }
+            if (!WS.wsDict[url]) {
+                WS.wsDict[url] = new WS(url);
+            }
+            return WS.wsDict[url];
+        }
+
+        connect = () => {
+            console.log(this.url, ": connecting web-socket");
+            this.ws = new WebSocket(this.url);
+            this.ws.binaryType = "arraybuffer";
+
+            this.ws.onopen = this.onOpen;
+            this.ws.onclose = this.onClose;
+            this.ws.onerror = this.onError;
+            this.ws.onmessage = this.onMessage;
+        }
+
+        disconnect = () => {
+            console.log(this.url, ": disconnecting web-socket");
+            this.shouldReconnect = false;
+            if (this.ws != null) {
+                this.ws.close();
+                this.ws = null;
+            } else {
+                this.resetReconnect();
+            }
+        }
+
+        reconnect = () => {
+            console.log(this.url, ": reconnecting web-socket in", this.reconnectDelay / 1000, "seconds...");
+            this.ws = null;
+            this.reconnectTimeout = setTimeout(() => {
+                if (this.shouldReconnect) {
+                    this.backoffReconnectDelay();
+                    this.connect();
+                }
+            }, this.reconnectDelay);
+        }
+
+        resetReconnect = () => {
+            if (isDefined(this.reconnectTimeout) && this.reconnectTimeout != null) {
+                clearTimeout(this.reconnectTimeout);
+            }
+            this.shouldReconnect = true;
+            this.reconnectDelay = 1000;
+            this.reconnectTimeout = null;
+        }
+
+        backoffReconnectDelay = () => {
+            this.reconnectDelay = Math.min(this.reconnectDelay * 2, 30000);
+        }
+
+        onOpen = () => {
+            console.log("WebSocket connected");
+            this.resetReconnect();
+        }
+
+        onClose = () => {
+            console.log("WebSocket disconnected");
+            if (this.shouldReconnect) {
+                this.reconnect();
+            } else {
+                this.resetReconnect();
+            }
+        }
+
+        onError = (error) => {
+            console.log("WebSocket error:", error);
+        }
+
+        onMessage = (event) => {
+            if (event.data instanceof ArrayBuffer) {
                 try {
-                    sourceBuffer.remove(0, mediaSource.duration);
-                    console.log("Cleared SourceBuffer");
+                    const json = JSON.parse(new TextDecoder("utf-8").decode(event.data.slice(0, 100)));
+                    const data = new Uint8Array(event.data.slice(100));
+                    this.ifVPExists(this.client(json), (vp) => {
+                        vp.onWSMessage({
+                            json: json,
+                            data: data
+                        });
+                    });
                 } catch (e) {
-                    console.error("Error clearing SourceBuffer:", e);
+                    console.log("Error reading received message (ArrayBuffer):", e);
+                }
+            } else {
+                try {
+                    const json = JSON.parse(event.data);
+                    this.ifVPExists(this.client(json), (vp) => {
+                        vp.onWSMessage({
+                            json: json
+                        });
+                    });
+                } catch (e) {
+                    console.log("Unknown message received:", event.data);
                 }
             }
-            mediaSource.endOfStream();
-            video.src = "";
-            mediaSource = null;
-            sourceBuffer = null;
-            console.log("MediaSource deinitialized");
+        }
+
+        isOpen = () => {
+            return (this.ws != null && this.ws.readyState === this.ws.OPEN);
+        }
+        
+        send = (vp, json) => {
+            json["client"] = vp.id();
+            this.ws.send(JSON.stringify(json));
+        }
+
+        registerVP = (vp) => {
+            if (Object.keys(this.vpDict).length === 0) {
+                this.connect();
+            }
+            this.vpDict[vp.id()] = vp;
+        }
+
+        unregisterVP = (vp) => {
+            delete this.vpDict[vp.id()];
+            if (Object.keys(this.vpDict).length === 0) {
+                this.disconnect();
+            }
+        }
+
+        vp = (id) => {
+            return this.vpDict[id];
+        }
+
+        vpExists = (id) => {
+            return isDefined(this.vp(id));
+        }
+
+        ifVPExists = (id, cb) => {
+            if (this.vpExists(id) === false) {
+                console.log("VP client id not exists:", id);
+                return;
+            }
+            cb(this.vp(id));
+        }
+
+        client = (json) => {
+            return json["client"];
         }
     }
+    
+    class VideoPlayer {
+        constructor (element) {
+            console.log("constructing: ", element.attr("id"));
 
-    function startStreaming() {
-        ws = new WebSocket("ws://localhost:12345");
+            this.element = element;
+            this.mediaSource = null;
+            this.sourceBuffer = null;
 
-        ws.binaryType = "arraybuffer";
+            this.autoPlay = false;
+            if (isDefined(this.element.data("autoplay"))) {
+                this.autoPlay = true;
+            }
+            this.isPaused = !this.autoPlay;
 
-        ws.onopen = () => {
-            streamingBtn.textContent = "Stop Streaming";
-            status.textContent = "Status: Connected to WebSocket";
-            console.log("WebSocket connected");
-            initializeMediaSource();
-            isStreaming = true;
-        };
+            this.startTimestampEpoch = Number(this.element.data("starttimestampepoch"));
+            this.durationSec = Number(this.element.data("durationsec"));
+            this.endTimestampEpoch = this.startTimestampEpoch + this.durationSec - 1;
+            // currentTimestamp = startTimestamp;
+            this.segmentDurationSec = Number(this.element.data("segmentdurationsec"));
+            this.startTime = -1 * this.segmentDurationSec * this.startTimestampEpoch;
+            this.endTime = this.segmentDurationSec * (this.endTimestampEpoch - this.startTimestampEpoch);
 
-        ws.onmessage = (event) => {
-            if (event.data instanceof ArrayBuffer) {
-                console.log("Received media segment (ArrayBuffer)");
+            $(this.element)[0].addEventListener('error', this.handleEvent);
+            $(this.element)[0].addEventListener('canplay', this.handleEvent);
+            $(this.element)[0].addEventListener('pause', this.handleEvent);
+            $(this.element)[0].addEventListener('play', this.handleEvent);
 
-                if (!sourceBuffer || sourceBuffer.updating) {
-                    console.log("SourceBuffer is not ready or updating");
+            const webSocketURL = this.element.data("websocketurl");
+            this.ws = WS.getInstance(webSocketURL);
+            this.ws.registerVP(this);
+        }
+
+        handleEvent = (event) => {
+            console.log(this.id(), ":", event);
+
+            if (event.type === 'error') {
+
+            } else if (event.type === 'canplay') {
+                if (this.autoPlay && !this.isPaused) {
+                    $(this.element)[0].play();
+                }
+            } else if (event.type === 'pause') {
+                this.isPaused = true;
+            } else if (event.type === 'play') {
+                this.isPaused = false;
+            } else {
+                console.log(this.id(), ": unhandled:", event.type);
+            }
+        }
+
+        init = () => {
+            if (!this.ws.isOpen()) {
+                console.log("WebSocket is not open");
+                return;
+            }
+
+            this.mediaSource = new MediaSource();
+
+            this.element.attr("src", URL.createObjectURL(this.mediaSource));
+
+            this.mediaSource.addEventListener("sourceopen", () => {
+                console.log(this.id(), ": media-source is open");
+                const mimeCodec = 'video/mp4; codecs="avc1.640028"';
+                this.sourceBuffer = this.mediaSource.addSourceBuffer(mimeCodec);
+                console.log(this.id(), ": source-buffer added:", mimeCodec);
+
+                this.sourceBuffer.timestampOffset = this.startTime;
+                this.mediaSource.duration = this.endTime;
+
+                this.sourceBuffer.addEventListener("updateend", () => {
+                    // mediaSource.endOfStream();
+                    // video.play();
+                    // console.log(this.id() + " " + this.mediaSource.readyState);
+                });
+
+                // video.addEventListener('timeupdate', checkBuffer);
+
+                this.sourceBuffer.addEventListener("error", (e) => {
+                    console.log("SourceBuffer error:", e);
+                });
+
+                this.ws.send(this, {
+                    type: "segment",
+                    segment: this.startTimestampEpoch.toString() + ".m4s"
+                });
+                // currentTimestamp++;
+            });
+
+            this.mediaSource.addEventListener("sourceended", () => {
+                console.log(this.id(), ": media-source ended");
+            });
+        }
+
+        destroy = () => {
+            this.ws.unregisterVP(this);
+
+            if (this.mediaSource) {
+                if (this.sourceBuffer) {
+                    if (!this.sourceBuffer.updating) {
+                        try {
+                            this.sourceBuffer.remove(0, this.mediaSource.duration);
+                            console.log(this.id(), ": cleared source-buffer");
+                        } catch (e) {
+                            console.log(this.id(), ": Error clearing SourceBuffer:", e);
+                        }
+                    }
+                    this.sourceBuffer = null;
+                }
+                this.mediaSource = null;
+                console.log(this.id(), ": media-source deinitialized");
+            }
+
+            this.element.attr("src", "");
+        }
+
+        id = () => {
+            return this.element.attr("id");
+        }
+
+        onWSMessage = ({json, data}={}) => {
+            if (isUndefined(data)) {
+                console.log(this.id(), ": JSON message received: ", json);
+            } else {
+                console.log(this.id(), ": Received media segment (ArrayBuffer)");
+
+                if (!this.sourceBuffer || this.sourceBuffer.updating) {
+                    console.log(this.id(), ": SourceBuffer is not ready or updating");
                     return;
                 }
-    
+
                 try {
-                    sourceBuffer.appendBuffer(new Uint8Array(event.data));
-                    console.log("Appended segment");
+                    this.sourceBuffer.appendBuffer(data);
+                    console.log(this.id(), ": Appended segment");
                 } catch (e) {
-                    console.error("Error appending segment:", e);
+                    console.log(this.id(), ": Error appending segment:", e);
                 }
 
-                if(current <= end){
-                    ws.send(JSON.stringify({
-                        type: "segment",
-                        segment: current.toString().padStart(3, '0') + ".m4s"
-                    }));
-                }
+                // if(current <= end){
+                //     ws.send({
+                //         type: "segment",
+                //         segment: current.toString().padStart(3, '0') + ".m4s"
+                //     });
+                // }
 
-                current++;
-
-            } else {
-                console.log("Non-binary message received:", event.data);
+                // current++;
             }
-        };
-
-        ws.onerror = (error) => {
-            console.error("WebSocket error:", error);
-            status.textContent = "Status: WebSocket error";
-        };
-
-        ws.onclose = () => {
-            console.log("WebSocket disconnected");
-            streamingBtn.textContent = "Start Streaming";
-            status.textContent = "Status: Disconnected from WebSocket";
-            deinitializeMediaSource();
-            isStreaming = false;
-        };
-    }
-
-    function stopStreaming() {
-        if (ws) {
-            ws.close();
-            console.log("WebSocket closed");
         }
     }
+
+    $(".video-player").each(function () {
+        let vp = new VideoPlayer($(this));
+        let button = $("<button></button>").attr("id", "btn_" + $(this).attr("id")).attr("type", "button").text("Start");
+        button.click(() => {
+            if (button.text() === "Start") {
+                button.text("Stop");
+                vp.init();
+            } else if (button.text() === "Stop") {
+                button.remove();
+                vp.destroy();
+                $(this).remove();
+            }
+        });
+        $(this).after( button);
+    });
 });
--- server/web-socket/server.py
+++ server/web-socket/server.py
@@ -1,22 +1,79 @@
+import argparse
 import asyncio
 import json
+import os
 import websockets
 
 
-async def handler(websocket):
-    async for message in websocket:
-        print(message)
-        request = json.loads(message)
-        if request.get("type") == "segment":
-            with open(f'../media/{request.get("segment")}', "rb") as f:
-                await websocket.send(f.read())
-        # await websocket.send(message)
+MAX_MESSAGE_LENGTH = 100
 
 
-async def main():
-    server = await websockets.serve(handler, 'localhost', 12345)
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="web-socket server command line arguments.")
+    
+    parser.add_argument("-p", "--port", type=int, required=True, help="port number.")
+    parser.add_argument("-d", "--directory", type=str, required=True, help="directory path.")
+    parser.add_argument("-v", "--verbose", action="store_true", help="enable verbose mode.")
+    
+    args = parser.parse_args()
+
+    port = args.port
+    directory = args.directory
+    verbose = args.verbose
+
+    if verbose:
+        print(f'port: {port}')
+        print(f'directory: {directory}')
+        print(f'verbose: {verbose}')
+    
+    return port, directory, verbose
+
+
+async def handler(websocket, directory, verbose):
+    client = websocket.remote_address
+    print(f"WebSocket connection opened: {client}")
+
+    try:
+        async for message in websocket:
+            if verbose:
+                print(message)
+            
+            request = json.loads(message)
+
+            if request.get("type") == "segment":
+                if len(message) > MAX_MESSAGE_LENGTH:
+                    if verbose:
+                        print(f'message too long: {message}: {len(message)}')
+                    request["error"] = "MessageTooLong"
+                    await websocket.send(json.dumps(request))
+                else:
+                    try:
+                        with open(os.path.join(directory, request.get("segment")), "rb") as f:
+                            file_data = f.read()
+                            additional_data = (message + (' ' * (MAX_MESSAGE_LENGTH - len(message)))).encode()
+                            combined_data = additional_data + file_data
+                            await websocket.send(combined_data)
+                    except FileNotFoundError as e:
+                        if verbose:
+                            print(f'file not found: {request.get("segment")}')
+                        request["error"] = "FileNotFound"
+                        await websocket.send(json.dumps(request))
+                    except Exception as e:
+                        if verbose:
+                            print(f'Exception: {e}')
+                        request["error"] = "Exception"
+                        await websocket.send(json.dumps(request))
+    finally:
+        print(f"WebSocket connection closed: {client}")
+
+
+async def main(port, directory, verbose):
+    server = await websockets.serve(lambda ws: handler(ws, directory, verbose), 'localhost', port)
+    if verbose:
+        print(f"Server started on port {port}")
     await server.wait_closed()
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    port, directory, verbose = parse_arguments()
+    asyncio.run(main(port, directory, verbose))
